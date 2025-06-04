ba0c20516d9d6e2a1daa531493e4da409ffaa8b3
cloud-engineer-2026 <cloudengineer2026@gmail.com>
2025-06-04 17:23:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-resource "tls_private_key" "ec2_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+resource "aws_key_pair" "jenkins_keypair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.private_key.public_key_openssh
 
-resource "aws_key_pair" "generated_key" {
-  key_name   = "my-ec2-key"
-  public_key = tls_private_key.ec2_key.public_key_openssh
-}
-
-resource "local_file" "private_key_pem" {
-  content         = tls_private_key.ec2_key.private_key_pem
-  filename        = "/Users/mmv0616/Documents/my-ec2-key.pem" # or your desired path
-  file_permission = "0600"
+  # Download keypair pem file to keypair path on local
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.private_key.private_key_pem}' > ${var.keypair_path}/${var.key_name}.pem" #; chmod 400 ${var.keypair_path}/${var.key_name}.pem"
+    interpreter = ["PowerShell", "-Command"]
+  }
+  
 }
 
 resource "aws_instance" "Docker_Instance" {
