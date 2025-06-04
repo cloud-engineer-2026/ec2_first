1337b10bfb80c8c30c1ba8eb6e66cd18e7ecd255
cloud-engineer-2026 <cloudengineer2026@gmail.com>
2025-06-04 17:01:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,7 @@
-resource "tls_private_key" "ec2_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "my-ec2-key"
-  public_key = tls_private_key.ec2_key.public_key_openssh
-}
-
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.ec2_key.private_key_pem
-  filename = "C:\Tickets\my-ec2-key.pem"
-  file_permission = "0600"
-}
 resource "aws_instance" "Docker_Instance" {
   instance_type = var.ec2_instance_type
   ami           = var.ami
-  key_name      = aws_key_pair.generated_key.key_name
+  keypair_path  = var.keypair_path
   tags = {
     Name = "DockerInstance"
   }
