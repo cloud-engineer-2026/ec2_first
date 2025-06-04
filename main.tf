bd306ae34690d82b3cff69f682acf4e97407fb25
cloud-engineer-2026 <cloudengineer2026@gmail.com>
2025-06-04 17:11:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "generated_key" {
- key_name         = var.key_name
- keypair_path     = var.keypair_path
+  key_name   = "my-ec2-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
+
+resource "local_file" "private_key_pem" {
+  content         = tls_private_key.ec2_key.private_key_pem
+  filename        = "/Tickets/moses/my-ec2-key.pem" # or your desired path
+  file_permission = "0600"
 }
 
 resource "aws_instance" "Docker_Instance" {
