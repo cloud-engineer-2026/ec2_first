4d3fcb0f11a83d3dba07052bd41648f105f6f5be
cloud-engineer-2026 <cloudengineer2026@gmail.com>
2025-06-04 21:48:35 +0530

    keypair2

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 resource "aws_key_pair" "Docker" {
   key_name   = var.key_name
   public_key = tls_private_key.private_key.public_key_openssh
